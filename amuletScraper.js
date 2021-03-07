8776d073c0067474d9f8f5d9fc06bc9c2c837095
Linch1 <47044850+Linch1@users.noreply.github.com>
2021-03-07 11:09:43 +0100

    Update amuletScraper.js

--- amuletScraper.js
+++ amuletScraper.js
@@ -1,10 +1,3 @@
-// const path = require('path');
-// function relativePath( path1, path2) {
-//   return path.relative(path.dirname(path1),path.dirname(path2));
-// } 
-// let relPath = relativePath('/WTM-VIEWS/lol.ts', '/WTM-VISUALS/navbar/ejs/assets/css/style.css');
-// console.log( relPath );
-
 const fs = require('fs');
 const crypto = require('crypto');
 function getHash(text) { return crypto.createHash('sha256').update(text).digest('hex');}
