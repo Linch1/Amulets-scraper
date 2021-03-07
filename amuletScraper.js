d88d7ea1a791e908e3ac2d5455ddffa9584f99b8
Linch1 <47044850+Linch1@users.noreply.github.com>
2021-03-07 12:03:11 +0100

    Update rarity check and output format

--- amuletScraper.js
+++ amuletScraper.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const { Console } = require('console');
+var regex = /(8)\1\1\1\1\1\1+/g;
+var RARITY = {
+    6: 'rare',
+    7: 'epic',
+    8: 'legendary',
+    9: 'mythic'
+}
+
 function getHash(text) { return crypto.createHash('sha256').update(text).digest('hex');}
-function checkValidAmulet( hash ) { return /(8)\1\1\1\1\1+/g.test(hash) }
+function checkValidAmulet( hash ) { return regex.test(hash) }
 function readFile(path) { return fs.readFileSync(path, "utf-8"); }
 function getFiles(path) { return fs.readdirSync(path, { withFileTypes: true }).filter((dirent) => dirent.isFile()).map((dirent) => dirent.name); }
 function createFile(path, content) { if (!fs.existsSync(path)) { writeFile(path, content); } }
@@ -18,9 +27,24 @@ function scrapeAmulet(text){
         while(getBytest(string) < 64 ){
             let hash = getHash(string);
             if(checkValidAmulet(hash)) {
-                appendFile(OUTPUT_FILE, POEM + ' |'+string + '|' + hash + '\n\n\n');
-                console.log( getBytest(string), unescape(encodeURIComponent(string)) )
-                console.log( 'FOUND: ', POEM + ' |'+string + '|' + hash );
+                let hashMatch = hash.match(regex);
+                let amuletRarityLength = hashMatch ? hashMatch[0].length : undefined;
+                let rarity = amuletRarityLength ? RARITY[amuletRarityLength] : "UNKNOWN";
+                if( !rarity ) rarity = 'legendary++'
+appendFile(OUTPUT_FILE, `
+AMULET: |${string}|
+RARITY_LENGTH: ${amuletRarityLength}
+RARITY: ${rarity}
+HASH: ${hash}
+BYTES: ${getBytest(string)}\n\n`);
+
+console.log( `
+FOUND IN : ${POEM},
+AMULET: |${string}|
+RARITY_LENGTH: ${amuletRarityLength}
+RARITY: ${rarity}
+HASH: ${hash}
+BYTES: ${getBytest(string)}`);
             }
             count ++;
             string = textAsArray.slice(0, count).join(" ");
@@ -29,9 +53,9 @@ function scrapeAmulet(text){
         textAsArray.shift(); // remove the analized string
     }
 }
-var MAIN_DIR = "/home/pero/projects/WTM/LIB/AMULETS"; // must not end with a '/';
+var MAIN_DIR = "/home/pero/projects/zora/AMULETS"; // must not end with a '/';
 let directories = getDirectories(MAIN_DIR);
-let skip = [];
+let skip = ['orwell', 'shakespeare', 'sparse'];
 var POEM;
 var OUTPUT_FILE;
 for ( let dir of directories ){
